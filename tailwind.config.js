6b1b3792bd723747e611445f7b801fd60a836fab
Abdul Hamid Mahi <87108050+AHMahi@users.noreply.github.com>
2023-12-09 16:49:09 +1100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#588CFE', // Add your custom primary color here
+      },
+    },
+  },
+  plugins: [],
+}
+
+
