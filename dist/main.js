e5e5f2fee80e56fe5541debc3fb7ba9cafa1b7f5
Abdul Hamid Mahi <87108050+AHMahi@users.noreply.github.com>
2024-02-08 07:15:22 +1100

    GSAP animation

---
+++ dist/main.js
@@ -0,0 +1,25 @@
+function section1() {
+    let tl = gsap.timeline();
+
+    tl.from(".navbar, .navbar-links, .navbar-logo",{
+        opacity: 0,
+        y: -80,
+        stagger: 0.1,
+    });
+
+    tl.from(".hero-intro, .hero-title, .hero-desc, .hero-buttons",
+    {
+        opacity: 0,
+        x: -150,
+        stagger: 0.1,
+    });
+
+    tl.from(".home-scroll-mouse, .home-scroll-text, .home-scroll-box",{
+        opacity: 0,
+        y: -30,
+        stagger: 0.1,
+    });
+
+}
+
+section1();
