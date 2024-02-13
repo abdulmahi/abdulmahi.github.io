d8c911105eee5a9990b52563846caa103c083527
Abdul Hamid Mahi <87108050+AHMahi@users.noreply.github.com>
2024-02-13 20:11:20 +1100

    added bloodSync mockup

--- dist/main.js
+++ dist/main.js
@@ -27,6 +27,33 @@ function section1() {
         stagger: 0.1,
     });
 
+    
+
 }
 
+function section2() {
+    gsap.registerPlugin(ScrollTrigger); // Register the ScrollTrigger plugin
+
+    let t2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".services", // Replace with your section's class or ID
+            start: "top center", // Animation will start when the top of the section hits the center of the viewport
+            // You can adjust "start" and "end" according to your needs
+            end: "bottom center",
+            toggleActions: "play none none none", // Defines what happens to the animation at start, end, enter, and leave. Here, it only plays at the start.
+            // You can use "restart pause resume reverse" or other combinations based on your requirements
+        }
+    });
+
+    t2.from(".services, service-text-container, .service-container, .service-heading, .service-desc, .service-hr", {
+        opacity: 0,
+        y: -80,
+        stagger: 0.2,
+    });
+}
+
+
+
+
 section1();
+section2();
